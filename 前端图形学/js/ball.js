b3c4ebed0d3a5860a321f996dc6f115487bfd3c6
= <aifly_xc@163.com>
2019-07-04 18:11:40 +0800

    a

--- 前端图形学/js/ball.js
+++ 前端图形学/js/ball.js
@@ -4,15 +4,21 @@ class Ball {
 		this.x = 0;//小球x坐标
 		this.y = 0;//小球y坐标
 		this.r = 20;//小球半径
+		
 		this.scaleX = 1;//小球x轴的缩放
 		this.scaleY = 1;//小球y轴的缩放
 		this.strokeStyle = 'rgba(0,0,0,0)';//小球的描边颜色
 		this.fillStyle = '#abcdef';//小球填充颜色
 		this.opacity = 1;//小球透明度
 	
-			this.g  = 3;//小于的重力加速度
+		this.g  = 3;//小于的重力加速度
+
+		this.x3d = 0;
+		this.y3d = 0;
+		this.z3d = 0;
 		this.vx = 0;
 		this.vy = 0;
+		this.vz = 0;
 		Object.assign(this, options);//将实例化的参数覆盖默认参数
 		return this;//返回当前对象，以实现链式调用
 	}

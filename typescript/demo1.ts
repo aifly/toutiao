7e28d8a1b35823522d377bf6424e657170dc4399
徐畅 <xuchang@bogon.dhcp>
2019-09-08 21:34:14 +0800

    a

---
+++ typescript/demo1.ts
@@ -0,0 +1,24 @@
+
+var anything :Number | string = '1234';
+var anything1 :Number | string = 1234;
+
+
+
+function alertName(): void {
+    alert('My name is Tom');
+}
+
+
+
+
+//通过void来定义的变量
+let unusable: void = undefined;
+let unusable1: void = null;
+var num1:number=  unusable;//报错
+var num2:number=  unusable1;
+
+let un:undefined = undefined;
+var num3:number = un;//不报错
+
+
+
